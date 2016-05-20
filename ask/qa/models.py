242d56f7264b0a870af8815f833bf4ba878eacb6
denv3r <misterdenver@gmail.com>
2016-05-20 22:32:11 +0300

    models added

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Question(models.Model):
+	title = models.CharField()
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	rating = models.models.IntegerField()
+	author = models.ForeignKey(User, on_delete=models.SET_NULL)
+	likes = ManyToManyField(User)
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	question = models.ForeignKey(Question, on_delete=models.SET_NULL)
+	author = models.ForeignKey(Question, on_delete=models.SET_NULL)
