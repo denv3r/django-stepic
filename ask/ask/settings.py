a4b20bbc0ab9cf3663d2af3ff95465b4484be187
denv3r <misterdenver@gmail.com>
2016-05-20 23:45:27 +0300

    fix

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
 		'NAME': 'django',
         'USER': 'root',
-        'PASSWORD': ''
+        'PASSWORD': '',
 		'HOST': '127.0.0.1'
     }
 }
